6c314e665ad4a95ea2c514fad5e8bcd64f728db1
Michael DeAngelo <deangelomp@gmail.com>
2022-10-17 07:09:44 -0500

    optional subcommand

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use vergen::{Config, vergen, ShaKind};
 
 fn main() {
     /*
@@ -10,4 +11,10 @@ fn main() {
     */
     let dt = Local::now();
     println!("cargo:rustc-env=BUILD_INFO={}", dt.format("%Y-%m-%dT%H:%M:%S%.f%:z"));
+
+    let mut config = Config::default();
+
+    // Change the SHA output to the short variant
+    *config.git_mut().sha_kind_mut() = ShaKind::Short;
+    vergen(config).expect("kjlkjl");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 mod matches;
 
-use clap::Parser;
+use std::process::exit;
+use clap::{Parser, Args, Subcommand};
 use anyhow::{Context, Result};
 use matches::find_matches;
-use std::env;
+
 
 /// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version)]
-#[clap(about, long_about = None)]
+#[command(about, long_about = None)]
+#[command(propagate_version = true)]
 struct Cli {
+    /// The pattern to look for
+    // pattern: String,
+    /// The path to the file to read
+    // #[arg()]
+    // path: std::path::PathBuf,
+
+    #[command(subcommand)]
+    // #[clap(required=false)]
+    command: Option<Commands>,
+
+    #[arg(short, long)]
+    build: bool,
+
+    #[arg(short='x', long)]
+    experimental: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Search(Search),
+}
+
+#[derive(Args)]
+struct Search {
     /// The pattern to look for
     pattern: String,
     /// The path to the file to read
@@ -19,6 +45,8 @@ struct Cli {
 
 fn main() -> Result<()> {
 
+
+
     // println!("Build TimeStamp: {}", env!("BUILD_INFO"));
     //
     // let version = option_env!("PROJECT_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"));
@@ -26,11 +54,42 @@ fn main() -> Result<()> {
 
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{:?}`", args.path))?;
+    if args.build {
+       buildInfo();
+        exit(0);
+    }
 
+    // match on the commands
+    match &args.command {
+        // args.command is of type Option, check for both some and none
+        Some(Search) => {
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+            // we have something, now match on its type and handle it
+            match &Search {
+                Commands::Search(Search) => {
+                    println!("{}", Search.pattern);
+                    println!("{}", Search.path.display());
+
+                    let content = std::fs::read_to_string(&Search.path)
+                        .with_context(|| format!("could not read file `{:?}`", Search.path))?;
+
+                    find_matches(&content, &Search.pattern, &mut std::io::stdout());
+                }
+            }
+        }
+
+        // what's best here, just exit?
+        None => {
+            println!("wtf!")
+        }
+    }
 
     Ok(())
+}
+
+fn buildInfo() {
+    println!("Build Timestamp: {}", env!("VERGEN_BUILD_TIMESTAMP"));
+    println!("git semver: {}", env!("VERGEN_GIT_SEMVER"));
+    println!("git branch: {}", env!("VERGEN_GIT_BRANCH"));
+    println!("git SHA SHORT: {}", env!("VERGEN_GIT_SHA_SHORT"));
 }
